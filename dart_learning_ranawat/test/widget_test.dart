2eb4b0406d2eb41e86244ff044ccb38453add48e
Robins K.T <karanithiaine4@gmail.com>
2024-05-28 11:54:43 +0300

    add functions

--- dart_learning_ranawat/test/widget_test.dart
+++ dart_learning_ranawat/test/widget_test.dart
@@ -13,6 +13,7 @@ import 'package:dart_learning_ranawat/data_types.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
+    //
     //await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
