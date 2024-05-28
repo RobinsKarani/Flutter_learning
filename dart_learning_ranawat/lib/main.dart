816b47f6118a2da516ab00d011ecaf4b3bcf6ee5
Robins K.T <karanithiaine4@gmail.com>
2024-05-28 11:52:49 +0300

    add functions

--- dart_learning_ranawat/lib/main.dart
+++ dart_learning_ranawat/lib/main.dart
@@ -10,10 +10,15 @@
 //   print(someValue?.length);
 // }
 void main() {
-  String name = PrintName();
-  print(name);
+  var (age, firstName, isAdult, lastName) = printName();
+  print("$firstName $lastName, Age: $age, Is Adult: $isAdult");
 }
 
-String PrintName() {
-  return 'Hello World';
+(int, String, bool, String) printName() {
+  int age = 18;
+  String firstName = 'Robins';
+  String lastName = 'Thiaine';
+  bool isAdult = age >= 18; // Check if age is greater than or equal to 18
+
+  return (age, firstName, isAdult, lastName);
 }
