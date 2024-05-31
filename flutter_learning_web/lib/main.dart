9c044e9851de7bc1196b4718d8071f969070f9e4
Robins K.T <karanithiaine4@gmail.com>
2024-05-31 12:44:39 +0300

    Providing Default Value On Positional Parameter

--- flutter_learning_web/lib/main.dart
+++ flutter_learning_web/lib/main.dart
@@ -1,9 +1,11 @@
-void printInfo(String name, String gender, [String title = "sir/ma'am"]) {
-  print("Hello $title $name your gender is $gender.");
+void add(int num1, int num2, [int num3 = 0]) {
+  int sum;
+  sum = num1 + num2 + num3;
+
+  print("The sum is $sum");
 }
 
 void main() {
-  printInfo("John", "Male");
-  printInfo("John", "Male", "Bw.");
-  printInfo("Kavya", "Female", "Bi.");
+  add(10, 20); //num3 is zero
+  add(10, 20, 30); //num3 is reassigned to 30
 }
