3d21222b8aa65c07a293321a7399cdda843b184a
Robins K.T <karanithiaine4@gmail.com>
2024-06-13 08:58:28 +0300

    default constructor

---
+++ flutter_learning_web/lib/default_constructor.dart
@@ -0,0 +1,14 @@
+class Laptop {
+  String? brand;
+  int? price;
+
+  // Constructor
+  Laptop() {
+    print('this is the default constructor');
+  }
+}
+
+void main() {
+  // Here laptop is object of class Laptop.
+  Laptop laptop = Laptop();
+}
