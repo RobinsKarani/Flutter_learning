113ebe8b55d830f024b717a8dcb3ad9bab035230
Robins K.T <karanithiaine4@gmail.com>
2024-06-15 21:15:03 +0300

    polymorphism using method override~means of transport example

--- flutter_learning_web/lib/polymorphism/method_override.dart
+++ flutter_learning_web/lib/polymorphism/method_override.dart
@@ -1,20 +1,39 @@
-class Animal {
-  void eat() {
-    print("Animal is eating");
+// General Toy class with a move method
+class Toy {
+  void move() {
+    print('Move forward');
   }
 }
 
-class Dog extends Animal {
+// Car class that overrides the move method
+class Car extends Toy {
+  void move() {
+    print('Drive on wheels');
+  }
+}
+
+// Plane class that overrides the move method
+class Plane extends Toy {
   @override
-  void eat() {
-    print("Dog is eating");
+  void move() {
+    print('Fly in the sky');
+  }
+}
+
+// Boat class that overrides the move method
+class Boat extends Toy {
+  @override
+  void move() {
+    print('Sail on water');
   }
 }
 
 void main() {
-  Animal animal = Animal();
-  animal.eat();
+  Toy myCar = Car();
+  Toy myPlane = Plane();
+  Toy myBoat = Boat();
 
-  Dog dog = Dog();
-  dog.eat();
+  myCar.move(); // Output: Drive on wheels
+  myPlane.move(); // Output: Fly in the sky
+  myBoat.move(); // Output: Sail on water
 }
